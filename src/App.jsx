f907694b39c0666cfb6ee797a4ac3d0cd4eddcf8
ranger <785799675@qq.com>
2023-02-15 00:04:00 +0800

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [flag2, setFlag2] = useState(false)
   const [flag, setFlag] = useState(false)
   const [openflag, setOpenflag] = useState(true)
   useEffect(()=>{
@@ -17,11 +18,12 @@ function App() {
     <div className="App w-screen h-screen bg-fuchsia-100">
       {openflag?
         <Loading />:
-        <div className='w-screen h-screen flex flex-col items-center justify-center flex-nowrap'>
-            <button onClick={()=>{setFlag(true)}} className="btn">点我一下</button>
+        <div className='w-screen h-screen flex flex-col items-center justify-around flex-nowrap'>
+            <button onClick={()=>{setFlag2(true)}} className="btn">点我一下</button>
+            {flag2&& <button onClick={()=>{setFlag(true)}} className="btn">再点一下嘛</button> }
             {flag&&
               <div className='mt-1'>
-                <p>情人节快乐</p>
+                <p className='text-slate-700'>情人节快乐!!</p>
               </div>
             }
         </div>
