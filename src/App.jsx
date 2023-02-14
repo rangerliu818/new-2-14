e2c40bc90c86c85023541c4ce1718a043c727e37
ranger <785799675@qq.com>
2023-02-14 22:55:15 +0800

    change style

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ function App() {
   const [flag, setFlag] = useState(false)
 
   return (
-    <div className="App">
-        <button onClick={()=>{setFlag(true)}} className="btn">点我一下</button>
-        {flag&&
-          <div>
-            <p>情人节快乐</p>
-          </div>
-        }
+    <div className="App w-screen h-screen bg-fuchsia-100">
+      <div className='w-screen h-screen flex flex-col items-center justify-center flex-nowrap'>
+          <button onClick={()=>{setFlag(true)}} className="btn">点我一下</button>
+          {flag&&
+            <div className='mt-1'>
+              <p>情人节快乐</p>
+            </div>
+          }
+      </div>
     </div>
   )
 }
